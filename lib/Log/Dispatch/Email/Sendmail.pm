7e60e009e25fca9f347aa4acb95283658d55cc93
Nigel Horne <njh@bandsman.co.uk>
2023-11-20 07:40:37 -0500

    Release 0.04

--- lib/Log/Dispatch/Email/Sendmail.pm
+++ lib/Log/Dispatch/Email/Sendmail.pm
@@ -11,11 +11,11 @@ Log::Dispatch::Email::Sendmail - Subclass of Log::Dispatch::Email that sends e-m
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
